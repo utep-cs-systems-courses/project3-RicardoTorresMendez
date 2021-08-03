7f13b8d39e82ed811e783e641e608441e5f34b01
RicardoTorresMendez <josertorresm1996@gmail.com>
2021-08-03 07:54:01 -0500

    test

--- wakedemo/wakedemo.c
+++ wakedemo/wakedemo.c
@@ -133,12 +133,12 @@ void draw_hourglass_sand(){
   if( layer == total ){
     layer = 0;
     state = 3;
-    fillRectangle( 40, 63, 1, 40, COLOR_GREEN_YELLOW );
+    fillRectangle( 80, 63, 1, 40, COLOR_GREEN_YELLOW );
     buzzer_set_period( 300 );
     return;
   }
 
-  fillRectangle( 40, 63, 1, 40, COLOR_GRAY );
+  fillRectangle( 80, 63, 1, 40, COLOR_GRAY );
 
   fillRectangle( col + layer, row + layer, width - layer - layer, 1, COLOR_GREEN_YELLOW);
 
