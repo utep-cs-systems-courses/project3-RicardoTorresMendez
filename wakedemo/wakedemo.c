65c3244110b909a9545a10d54417ff78b1f630e3
RicardoTorresMendez <josertorresm1996@gmail.com>
2021-08-02 23:01:14 -0500

    test

--- wakedemo/wakedemo.c
+++ wakedemo/wakedemo.c
@@ -85,8 +85,8 @@ void main(){
 void draw_hourglass();
 
 void draw_hourglass(){
-  fillRectangle(10,1, 104, 2, COLOR_WHITE);
-  fillRectangle(10,120, 104, 2, COLOR_WHITE);
+  fillRectangle(10,1, 105, 2, COLOR_WHITE);
+  fillRectangle(10,120, 105, 2, COLOR_WHITE);
 }
 
 /*
